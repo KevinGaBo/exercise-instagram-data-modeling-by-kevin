331935237a167d0e89fed42b82d3760b00d1ea4e
Kevin Gabriel Boriosi <153501399+KevinGaBo@users.noreply.github.com>
2024-06-04 13:33:31 +0000

    finalizado tablas de modelos

--- src/models.py
+++ src/models.py
@@ -1,29 +1,59 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+    is_active = Column(Boolean(), unique=False, nullable=False)
+    profile = relationship('Profile', back_populates = 'user', uselist=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Profile(Base):
+    __tablename__ = 'profile'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(120), unique=False, nullable=False)
+    nick_name = Column(String(80), unique=False, nullable=False)
+    posts = Column(String(80), unique=False, nullable=False)
+    follows = Column(String(80), unique=False, nullable=False)
+    followers = Column(String(80), unique=False, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='profile')
+    history = relationship('History', back_populates='profile')
+    post = relationship('Post', back_populates='profile')
+
+
+class History(Base):
+    __tablename__ = 'history'
+    id = Column(Integer, primary_key=True)
+    history_photo = Column(String(120), unique=True, nullable=False)
+    history_reel = Column(String(80), unique=False, nullable=False)
+    history_link = Column(String(80), unique=False, nullable=False)
+    history_tag = Column(String(80), unique=False, nullable=False)
+    profile_id = Column(Integer, ForeignKey('profile.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    profile = relationship('Profile', back_populates='history')
+    post = relationship('Post', back_populates='history')
+    
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    photo = Column(String(120), unique=True, nullable=False)
+    reel = Column(String(80), unique=False, nullable=False)
+    likes = Column(String(80), unique=False, nullable=False)
+    comments = Column(String(80), unique=False, nullable=False) 
+    profile_id = Column(Integer, ForeignKey('profile.id'))
+    profile = relationship('Profile', back_populates='post')
+    history = relationship('History', back_populates='post')
+
 
     def to_dict(self):
         return {}
